34e67096d6b52591d1c315d153e30270df274535
Dannie Fu <51844046+chuxiaofdn@users.noreply.github.com>
2021-12-05 22:37:35 -0800

    peer review update

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,9 +1,21 @@
 from typing import Dict
+from typing import Tuple
 from typing import Union
 
 from sample_size.sample_size_calculator import SampleSizeCalculator
 
 DEFAULT_ALPHA = 0.05
+METRIC_PARAMETERS = {
+    "boolean": {"probability": "baseline probability (between 0 and 1)"},
+    "numeric": {"variance": "variance of the baseline metric"},
+    "ratio": {
+        "numerator_mean": "mean of the baseline metric's numerator",
+        "numerator_variance": "variance of the baseline metric's numerator",
+        "denominator_mean": "mean of the baseline metric's denominator",
+        "denominator_variance": "variance of the baseline metric's denominator",
+        "covariance": "covariance between the baseline metric's numerator and denominator",
+    },
+}
 
 
 def is_float(value: str) -> float:
@@ -18,7 +30,7 @@ def get_raw_input(text: str) -> str:
     return input(text)
 
 
-def get_input(input_name: str) -> float:
+def get_float_input(input_name: str) -> float:
     input_str = get_raw_input(f"Enter the {input_name}: ")
     if is_float(input_str):
         return float(input_str)
@@ -29,22 +41,26 @@ def get_input(input_name: str) -> float:
 def get_alpha() -> Union[float, None]:
     alpha_default_check = get_raw_input("Do you want to use default alpha (0.05) for the power analysis? (y/n)")
     if alpha_default_check.lower() == "n":
-        alpha = get_input("alpha")
-        if 0 <= alpha <= 0.3:
+        alpha = get_float_input("alpha (between 0 and 0.3 inclusively)")
+        if 0 < alpha <= 0.3:
+            print(f"Using alpha ({alpha}) and default power (0.8)...")
             return alpha
         else:
             raise Exception("Error: Please provide a float between 0 and 0.3 for alpha.")
     else:
-        print("Using default alpha...")
+        print("Using default alpha (0.05) and power (0.8)...")
         return None
 
 
 def get_mde(metric_type: str) -> float:
-    mde = get_input(f"absolute minimum detectable effect for this {metric_type}")
+    mde = get_float_input(
+        f"absolute minimum detectable effect for this {metric_type} \n"
+        f"MDE: targeted treatment metric value minus the baseline value"
+    )
     return mde
 
 
-def get_variable_from_input() -> str:
+def get_metric_type_from_input() -> str:
     metric_type = get_raw_input("Enter metric type (Boolean, Numeric, Ratio): ").strip().lower()
     if metric_type in ["boolean", "numeric", "ratio"]:
         return metric_type
@@ -57,40 +73,29 @@ def get_variable_parameters(parameter_definitions: Dict[str, str]) -> Dict[str, 
     parameters = {}
 
     for parameter_name, parameter_definition in parameter_definitions.items():
-        parameters[parameter_name] = get_input(parameter_definition)
+        parameters[parameter_name] = get_float_input(parameter_definition)
 
     return parameters
 
 
-def register_metric(metric_type: str, calculator: SampleSizeCalculator) -> None:
-    metric_type = metric_type.lower()
-    if metric_type == "boolean":
-        parameters_definitions = {"probability": "baseline probability"}
-        parameters = get_variable_parameters(parameters_definitions)
-        mde = get_mde(metric_type)
-        calculator.register_bool_metric(parameters["probability"], mde)
-    elif metric_type == "numeric":
-        parameters_definitions = {"variance": "variance of the baseline metric"}
-        parameters = get_variable_parameters(parameters_definitions)
-        mde = get_mde(metric_type)
-        calculator.register_numeric_metric(parameters["variance"], mde)
-    elif metric_type == "ratio":
-        parameters_definitions = {
-            "numerator_mean": "mean of the baseline metric's numerator",
-            "numerator_variance": "variance of the baseline metric's numerator",
-            "denominator_mean": "mean of the baseline metric's denominator",
-            "denominator_variance": "variance of the baseline metric's denominator",
-            "covariance": "covariance between the baseline metric's numerator and denominator",
-        }
-        parameters = get_variable_parameters(parameters_definitions)
-        mde = get_mde(metric_type)
-        calculator.register_ratio_metric(
-            parameters["numerator_mean"],
-            parameters["numerator_variance"],
-            parameters["denominator_mean"],
-            parameters["denominator_variance"],
-            parameters["covariance"],
-            mde,
-        )
-    else:
-        raise Exception("Error: Unexpected variable name. Please use Boolean, Numeric, or Ratio.")
+def get_metric_metadata_from_input() -> Tuple[str, Dict[str, float]]:
+    metric_type = get_metric_type_from_input().lower()
+    metric_metadata = get_variable_parameters(METRIC_PARAMETERS[metric_type])
+    metric_metadata["mde"] = get_mde(metric_type)
+
+    return metric_type, metric_metadata
+
+
+def register_metric(
+    metric_type: str,
+    metric_metadata: Dict[str, float],
+    calculator: SampleSizeCalculator,
+) -> None:
+    VAR_REGISTER_FUNC_MAP = {
+        "boolean": "register_bool_metric",
+        "numeric": "register_numeric_metric",
+        "ratio": "register_ratio_metric",
+    }
+
+    register_func = getattr(calculator, VAR_REGISTER_FUNC_MAP[metric_type])
+    register_func(**metric_metadata)
--- tests/test_scripts/test_sample_size_run.py
+++ tests/test_scripts/test_sample_size_run.py
@@ -1,55 +1,108 @@
 import unittest
+from io import StringIO
 from unittest.mock import patch
 
 from numpy.testing import assert_equal
 
+from sample_size.sample_size_calculator import DEFAULT_ALPHA
 from sample_size.sample_size_calculator import SampleSizeCalculator
 from scripts.sample_size_run import main
 
 
 class TestMain(unittest.TestCase):
-    @patch("sample_size.sample_size_calculator.SampleSizeCalculator.get_overall_sample_size")
+    def setUp(self):
+        self.DEFAULT_METRIC_TYPE = "boolean"
+        self.DEFAULT_METRIC_METADATA = {"test": "case"}
+        self.DEFAULT_SAMPLE_SIZE = 2000
+
+    @patch("sample_size.sample_size_calculator.SampleSizeCalculator.get_sample_size")
     @patch("scripts.utils.register_metric")
-    @patch("scripts.utils.get_variable_from_input")
+    @patch("scripts.utils.get_metric_metadata_from_input")
     @patch("scripts.utils.get_alpha")
     def test_main_alpha_input(
-        self, mock_get_alpha, mock_get_variable_from_input, mock_register_metric, mock_get_overall_sample_size
+        self,
+        mock_get_alpha,
+        get_metric_metadata_from_input,
+        mock_register_metric,
+        mock_get_sample_size,
     ):
-        test_metric_type = "boolean"
         test_alpha = 0.01
         mock_get_alpha.return_value = test_alpha
-        mock_get_variable_from_input.return_value = test_metric_type
-        mock_get_overall_sample_size.return_value = 2000
+        get_metric_metadata_from_input.return_value = (self.DEFAULT_METRIC_TYPE, self.DEFAULT_METRIC_METADATA)
+        mock_get_sample_size.return_value = self.DEFAULT_SAMPLE_SIZE
 
-        main()
+        with patch("sys.stdout", new=StringIO()) as fakeOutput:
+            main()
+            self.assertEqual(
+                fakeOutput.getvalue().strip(),
+                "Sample size needed in each group: {:.3f}".format(self.DEFAULT_SAMPLE_SIZE),
+            )
+
         calculator = SampleSizeCalculator(test_alpha)
 
         mock_get_alpha.assert_called_once()
-        mock_get_variable_from_input.assert_called_once()
-        assert_equal(mock_register_metric.call_args[0][0], test_metric_type)
-        assert_equal(mock_register_metric.call_args[0][1].alpha, test_alpha)
-        assert_equal(mock_register_metric.call_args[0][1].power, calculator.power)
-        mock_get_overall_sample_size.assert_called_once()
+        get_metric_metadata_from_input.assert_called_once()
+        assert_equal(mock_register_metric.call_args[0][0], self.DEFAULT_METRIC_TYPE)
+        assert_equal(mock_register_metric.call_args[0][1], self.DEFAULT_METRIC_METADATA)
+        assert_equal(mock_register_metric.call_args[0][2].alpha, test_alpha)
+        assert_equal(mock_register_metric.call_args[0][2].power, calculator.power)
+        mock_get_sample_size.assert_called_once()
 
-    @patch("sample_size.sample_size_calculator.SampleSizeCalculator.get_overall_sample_size")
+    @patch("sample_size.sample_size_calculator.SampleSizeCalculator.get_sample_size")
     @patch("scripts.utils.register_metric")
-    @patch("scripts.utils.get_variable_from_input")
+    @patch("scripts.utils.get_metric_metadata_from_input")
     @patch("scripts.utils.get_alpha")
     def test_main_default_alpha(
-        self, mock_get_alpha, mock_get_variable_from_input, mock_register_metric, mock_get_overall_sample_size
+        self, mock_get_alpha, get_metric_metadata_from_input, mock_register_metric, mock_get_sample_size
     ):
-        test_metric_type = "boolean"
         test_alpha = None
         mock_get_alpha.return_value = test_alpha
-        mock_get_variable_from_input.return_value = test_metric_type
-        mock_get_overall_sample_size.return_value = 2000
+        get_metric_metadata_from_input.return_value = (self.DEFAULT_METRIC_TYPE, self.DEFAULT_METRIC_METADATA)
+        mock_get_sample_size.return_value = self.DEFAULT_SAMPLE_SIZE
 
-        main()
+        with patch("sys.stdout", new=StringIO()) as fakeOutput:
+            main()
+            self.assertEqual(
+                fakeOutput.getvalue().strip(),
+                "Sample size needed in each group: {:.3f}".format(self.DEFAULT_SAMPLE_SIZE),
+            )
+
         calculator = SampleSizeCalculator()
 
         mock_get_alpha.assert_called_once()
-        mock_get_variable_from_input.assert_called_once()
-        assert_equal(mock_register_metric.call_args[0][0], test_metric_type)
-        assert_equal(mock_register_metric.call_args[0][1].alpha, calculator.alpha)
-        assert_equal(mock_register_metric.call_args[0][1].power, calculator.power)
-        mock_get_overall_sample_size.assert_called_once()
+        get_metric_metadata_from_input.assert_called_once()
+        assert_equal(mock_register_metric.call_args[0][0], self.DEFAULT_METRIC_TYPE)
+        assert_equal(mock_register_metric.call_args[0][1], self.DEFAULT_METRIC_METADATA)
+        assert_equal(mock_register_metric.call_args[0][2].alpha, DEFAULT_ALPHA)
+        assert_equal(mock_register_metric.call_args[0][2].power, calculator.power)
+        mock_get_sample_size.assert_called_once()
+
+    @patch("sample_size.sample_size_calculator.SampleSizeCalculator.get_sample_size")
+    @patch("scripts.utils.register_metric")
+    @patch("scripts.utils.get_metric_metadata_from_input")
+    @patch("scripts.utils.get_alpha")
+    def test_main_exception_print(
+        self, mock_get_alpha, get_metric_metadata_from_input, mock_register_metric, mock_get_sample_size
+    ):
+        test_alpha = None
+        mock_get_alpha.return_value = test_alpha
+        get_metric_metadata_from_input.return_value = (self.DEFAULT_METRIC_TYPE, self.DEFAULT_METRIC_METADATA)
+        mock_get_sample_size.return_value = "test"
+
+        with patch("sys.stdout", new=StringIO()) as fakeOutput:
+            main()
+            self.assertEqual(
+                fakeOutput.getvalue().strip(),
+                "Error! The calculator isn't able to calculate sample size due to "
+                "\nUnknown format code 'f' for object of type 'str'",
+            )
+
+        calculator = SampleSizeCalculator()
+
+        mock_get_alpha.assert_called_once()
+        get_metric_metadata_from_input.assert_called_once()
+        assert_equal(mock_register_metric.call_args[0][0], self.DEFAULT_METRIC_TYPE)
+        assert_equal(mock_register_metric.call_args[0][1], self.DEFAULT_METRIC_METADATA)
+        assert_equal(mock_register_metric.call_args[0][2].alpha, DEFAULT_ALPHA)
+        assert_equal(mock_register_metric.call_args[0][2].power, calculator.power)
+        mock_get_sample_size.assert_called_once()
