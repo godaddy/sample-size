11a0c3ca92629b537eef251030350519c6cc06b4
xzou <xzou@godaddy.com>
2022-06-02 17:30:07 -0700

    fix unit tests

--- sample_size/multiple_testing.py
+++ sample_size/multiple_testing.py
@@ -23,17 +23,19 @@ class MultipleTestingMixin:
     power: float
     variants: int
 
-    def get_multiple_sample_size(self, lower: float, upper: float, depth: int = 0) -> int:
-        MAX_RECURSION_DEPTH = 20
-        EPSILON = 0.025
+    REPLICATION: int = 100
 
-        if depth > MAX_RECURSION_DEPTH:
-            raise RecursionError
+    def get_multiple_sample_size(self, lower: float, upper: float, depth: int = 0) -> int:
+        max_recursion_depth: int = 20
+        epsilon: float = 0.025  # TODO(any): make this configurable by users ML-5429
+
+        if depth > max_recursion_depth:
+            raise RecursionError(f"Couldn't find a sample size that satisfies the power you requested: {self.power}")
 
         candidate = int((upper + lower) / 2)
         expected_power = self._expected_average_power(candidate)
 
-        if np.isclose((self.power, expected_power), EPSILON) or np.isclose((candidate, upper), EPSILON):
+        if np.isclose(self.power, expected_power, epsilon):
             return candidate
 
         if expected_power > self.power:
@@ -41,12 +43,12 @@ class MultipleTestingMixin:
         else:
             return self.get_multiple_sample_size(candidate, upper, depth + 1)
 
-    def _expected_average_power(self, sample_size: int, REPLICATION: int = 100) -> float:
+    def _expected_average_power(self, sample_size: int, replication: int = REPLICATION) -> float:
         power = []
         m = len(self.metrics) * (self.variants - 1)
         for m1 in range(m):
             nulls = np.array([True] * m1 + [False] * (m - m1))
-            for _ in range(REPLICATION):
+            for _ in range(replication):
                 true_null = nulls[np.random.permutation(m)]
                 p_values = [
                     m.generate_p_value(true_null[i], sample_size, self.variants) for i, m in enumerate(self.metrics)
--- tests/sample_size/scripts/test_multiple_testing.py
+++ tests/sample_size/scripts/test_multiple_testing.py
@@ -1,104 +1,84 @@
 import unittest
-from unittest.mock import MagicMock
 from unittest.mock import patch
 
 from sample_size.sample_size_calculator import SampleSizeCalculator
 
 
 class MultipleTestingTestCase(unittest.TestCase):
-    @patch("sample_size.sample_size_calculator.SampleSizeCalculator._get_single_sample_size", side_effect=[100, 1000])
+    def setUp(self):
+        self.test_lower = 100
+        self.test_upper = 1000
+        self.test_metric_type = "boolean"
+        self.test_probability = 0.05
+        self.test_mde = 0.02
+        self.test_metric_metadata = {"probability": self.test_probability, "mde": self.test_mde}
+        self.test_metric = {"metric_type": self.test_metric_type, "metric_metadata": self.test_metric_metadata}
+
+    @patch(
+        "sample_size.sample_size_calculator.SampleSizeCalculator._get_single_sample_size",
+        side_effect=[100, 100, 1000, 1000],
+    )
     @patch("sample_size.multiple_testing.MultipleTestingMixin._expected_average_power", side_effect=[0.5, 0.8])
     def test_get_multiple_sample_size(self, mock_expected_average_power, mock_get_single_sample_size):
-        test_lower = 100
-        test_upper = 1000
-        test_metric_type = "boolean"
-        test_probability = 0.05
-        test_mde = 0.02
-        test_metric_metadata = {"probability": test_probability, "mde": test_mde}
         calculator = SampleSizeCalculator()
-        calculator.register_metrics(
-            [
-                {"metric_type": test_metric_type, "metric_metadata": test_metric_metadata},
-                {"metric_type": test_metric_type, "metric_metadata": test_metric_metadata},
-            ]
-        )
+        calculator.register_metrics([self.test_metric, self.test_metric])
         sample_size = calculator.get_sample_size()
         self.assertEqual(mock_get_single_sample_size.call_count, 4)
         self.assertEqual(mock_expected_average_power.call_count, 2)
-        self.assertEqual(sample_size, ((test_upper + test_lower) / 2 + test_upper) / 2)
+        self.assertEqual(sample_size, ((self.test_upper + self.test_lower) / 2 + self.test_upper) / 2)
 
-    @patch("sample_size.sample_size_calculator.SampleSizeCalculator._get_single_sample_size", side_effect=[100, 1000])
+    @patch(
+        "sample_size.sample_size_calculator.SampleSizeCalculator._get_single_sample_size",
+        side_effect=[100, 100, 1000, 1000],
+    )
     @patch("sample_size.multiple_testing.MultipleTestingMixin._expected_average_power")
     def test_get_multiple_sample_size_no_recursion(self, mock_expected_average_power, mock_get_single_sample_size):
-        test_lower = 100
-        test_upper = 1000
-        test_metric_type = "boolean"
-        test_probability = 0.05
-        test_mde = 0.02
-        test_metric_metadata = {"probability": test_probability, "mde": test_mde}
         mock_expected_average_power.return_value = 0.79
 
         calculator = SampleSizeCalculator()
-        calculator.register_metrics(
-            [
-                {"metric_type": test_metric_type, "metric_metadata": test_metric_metadata},
-                {"metric_type": test_metric_type, "metric_metadata": test_metric_metadata},
-            ]
-        )
+        calculator.register_metrics([self.test_metric, self.test_metric])
         sample_size = calculator.get_sample_size()
         self.assertEqual(mock_get_single_sample_size.call_count, 4)
-        mock_expected_average_power.assert_called_once_with(int((test_upper + test_lower) / 2))
-        self.assertEqual(sample_size, (test_upper + test_lower) / 2)
+        mock_expected_average_power.assert_called_once_with(int((self.test_upper + self.test_lower) / 2))
+        self.assertEqual(sample_size, (self.test_upper + self.test_lower) / 2)
 
-    @patch("sample_size.sample_size_calculator.SampleSizeCalculator._get_single_sample_size", side_effect=[100, 1000])
+    @patch(
+        "sample_size.sample_size_calculator.SampleSizeCalculator._get_single_sample_size",
+        side_effect=[100, 100, 1000, 1000],
+    )
     @patch("sample_size.multiple_testing.MultipleTestingMixin._expected_average_power")
     def test_get_multiple_sample_size_too_many_recursion(
         self, mock_expected_average_power, mock_get_single_sample_size
     ):
-        test_lower = 100
-        test_upper = 1000
-        test_metric_type = "boolean"
-        test_probability = 0.05
-        test_mde = 0.02
-        test_metric_metadata = {"probability": test_probability, "mde": test_mde}
+        test_power = 0.8
         mock_expected_average_power.return_value = 0.1
 
         calculator = SampleSizeCalculator()
-        calculator.register_metrics(
-            [
-                {"metric_type": test_metric_type, "metric_metadata": test_metric_metadata},
-                {"metric_type": test_metric_type, "metric_metadata": test_metric_metadata},
-            ]
-        )
+        calculator.register_metrics([self.test_metric, self.test_metric])
 
-        with self.assertRaises(Exception):
+        with self.assertRaises(Exception) as context:
             calculator.get_sample_size()
+
+        self.assertEqual(
+            str(context.exception),
+            f"Couldn't find a sample size that satisfies the power you requested: {test_power}",
+        )
 
         self.assertEqual(mock_get_single_sample_size.call_count, 4)
         mock_expected_average_power.assert_called()
 
+    @patch("numpy.isclose")
     @patch("sample_size.metrics.BooleanMetric.generate_p_value")
     @patch("statsmodels.stats.multitest.multipletests")
-    def test_expected_average_power(self, mock_multipletests, mock_generate_p_value):
-        test_lower = 100
-        test_upper = 1000
-
-        test_metric_type = "boolean"
-        test_probability = 0.05
-        test_mde = 0.02
-        test_metric_metadata = {"probability": test_probability, "mde": test_mde}
-        mock_generate_p_value.return_value = MagicMock()
-        mock_multipletests.return_value = [[1, 1, 1, 1, 0]]
+    def test_expected_average_power(self, mock_multipletests, mock_generate_p_value, mock_isclose):
+        mock_generate_p_value.return_value = 0.01
+        mock_multipletests.return_value = [1, 1, 1]
+        mock_isclose.return_value = True
 
         calculator = SampleSizeCalculator()
-        calculator.register_metrics(
-            [
-                {"metric_type": test_metric_type, "metric_metadata": test_metric_metadata},
-                {"metric_type": test_metric_type, "metric_metadata": test_metric_metadata},
-            ]
-        )
-        replication = len(calculator.metrics) * (calculator.variants - 1) * 100
+        calculator.register_metrics([self.test_metric, self.test_metric, self.test_metric])
+        replication = len(calculator.metrics) ** 2 * (calculator.variants - 1) * 100
 
-        sample_size = calculator.get_sample_size()
+        sample_size = calculator.get_multiple_sample_size(self.test_lower, self.test_upper)
         self.assertEqual(mock_generate_p_value.call_count, replication)
-        self.assertEqual(sample_size, (test_upper + test_lower) / 2)
+        self.assertEqual(sample_size, ((self.test_upper + self.test_lower) / 2))
