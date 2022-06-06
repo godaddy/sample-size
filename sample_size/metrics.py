fix formula errors

sample_size/metrics.py
@@ -1,6 +1,5 @@
 from abc import ABCMeta
 from abc import abstractmethod
-from typing import List
 from typing import Union
 
 import numpy as np
@@ -10,7 +9,6 @@ from statsmodels.stats.power import TTestIndPower
 
 
 class BaseMetric:
-
     __metaclass__ = ABCMeta
     mde: float
 
@@ -35,14 +33,13 @@ class BaseMetric:
             return number
 
     @abstractmethod
-    def generate_p_value(self, true_null: bool, sample_size: int, variants: int) -> List[float]:
+    def generate_p_value(self, true_null: bool, sample_size: int) -> float:
         """
         This method simulates registered metric's p-value(s). The output will later be applied to BH procedure
 
         Parameters:
-            true_null: whether the null hypothesis is true
+            true_null: whether the null hypothesis is true, length= variants-1
             sample_size: sample size used for simulations
-            variants: number of test variants, including control
 
         Returns:
             p-value(s): A list of the p-values generated with length variants-1
@@ -51,7 +48,6 @@ class BaseMetric:
 
 
 class BooleanMetric(BaseMetric):
-
     probability: float
     mde: float
 
@@ -78,18 +74,17 @@ class BooleanMetric(BaseMetric):
         else:
             raise ValueError("Error: Please provide a float between 0 and 1 for probability.")
 
-    def generate_p_value(self, true_null: bool, sample_size: int, variants: int) -> List[float]:
+    def generate_p_value(self, true_null: bool, sample_size: int) -> float:
         if true_null:
-            return [p_null for p_null in stats.uniform.rvs(0, 1, size=variants - 1)]
+            return float(stats.uniform.rvs(0, 1))
 
         else:
             effect_size = self.mde / np.sqrt(2 * self.variance / sample_size)
-            z_alt = stats.norm.rvs(loc=effect_size, size=variants - 1)
-            return [p_alt for p_alt in 2 * stats.norm.sf(np.abs(z_alt))]
+            z_alt = stats.norm.rvs(loc=effect_size)
+            return float(2 * stats.norm.sf(np.abs(z_alt)))
 
 
 class NumericMetric(BaseMetric):
-
     mde: float
 
     def __init__(
@@ -108,18 +103,17 @@ class NumericMetric(BaseMetric):
     def power_analysis_instance(self) -> TTestIndPower:
         return TTestIndPower()
 
-    def generate_p_value(self, true_null: bool, sample_size: int, variants: int) -> List[float]:
+    def generate_p_value(self, true_null: bool, sample_size: int) -> float:
         if true_null:
-            return [p_null for p_null in stats.uniform.rvs(0, 1, size=variants - 1)]
+            return float(stats.uniform.rvs(0, 1))
 
         else:
-            nc = np.sqrt(sample_size / 2) * self.mde / self.variance
-            t_alt = stats.nct.rvs(nc=nc, df=2 * (sample_size - 1), size=variants - 1)
-            return [p_alt for p_alt in 2 * (stats.t.sf(np.abs(t_alt), 2 * (sample_size - 1)))]
+            nc = np.sqrt(sample_size / 2 / self.variance) * self.mde
+            t_alt = stats.nct.rvs(nc=nc, df=2 * (sample_size - 1))
+            return float(2 * stats.t.sf(np.abs(t_alt), 2 * (sample_size - 1)))
 
 
 class RatioMetric(BaseMetric):
-
     numerator_mean: float
     numerator_variance: float
     denominator_mean: float
@@ -144,7 +138,6 @@ class RatioMetric(BaseMetric):
 
     @property
     def variance(self) -> float:
-
         variance = (
             self.numerator_variance / self.denominator_mean ** 2
             + self.denominator_variance * self.numerator_mean ** 2 / self.denominator_mean ** 4
@@ -157,11 +150,11 @@ class RatioMetric(BaseMetric):
     def power_analysis_instance(self) -> NormalIndPower:
         return NormalIndPower()
 
-    def generate_p_value(self, true_null: bool, sample_size: int, variants: int) -> List[float]:
+    def generate_p_value(self, true_null: bool, sample_size: int) -> float:
         if true_null:
-            return [p_null for p_null in stats.uniform.rvs(0, 1, size=variants - 1)]
+            return float(stats.uniform.rvs(0, 1))
 
         else:
             effect_size = self.mde / np.sqrt(2 * self.variance / sample_size)
-            z_alt = stats.norm.rvs(loc=effect_size, size=variants - 1)
-            return [p_alt for p_alt in 2 * stats.norm.sf(np.abs(z_alt))]
+            z_alt = stats.norm.rvs(loc=effect_size)
+            return float(2 * stats.norm.sf(np.abs(z_alt)))

sample_size/multiple_testing.py
@@ -23,7 +23,7 @@ class MultipleTestingMixin:
     power: float
     variants: int
 
-    REPLICATION: int = 100
+    REPLICATION: int = 500
 
     def get_multiple_sample_size(self, lower: float, upper: float, depth: int = 0) -> int:
         max_recursion_depth: int = 20
@@ -32,9 +32,8 @@ class MultipleTestingMixin:
         if depth > max_recursion_depth:
             raise RecursionError(f"Couldn't find a sample size that satisfies the power you requested: {self.power}")
 
-        candidate = int((upper + lower) / 2)
+        candidate = int(lower + (upper - lower) / 2)
         expected_power = self._expected_average_power(candidate)
-
         if np.isclose(self.power, expected_power, epsilon):
             return candidate
 
@@ -45,15 +44,24 @@ class MultipleTestingMixin:
 
     def _expected_average_power(self, sample_size: int, replication: int = REPLICATION) -> float:
         power = []
-        m = len(self.metrics) * (self.variants - 1)
-        for m1 in range(m):
-            nulls = np.array([True] * m1 + [False] * (m - m1))
+        num_of_tests = len(self.metrics) * (self.variants - 1)
+        for m1 in range(num_of_tests):
+            nulls = np.array([True] * m1 + [False] * (num_of_tests - m1))
             for _ in range(replication):
-                true_null = nulls[np.random.permutation(m)]
-                p_values = [
-                    m.generate_p_value(true_null[i], sample_size, self.variants) for i, m in enumerate(self.metrics)
-                ]
+                p_values = []
+                true_null = nulls[np.random.permutation(num_of_tests)]
+                for v in range(self.variants - 1):
+                    p_values.extend(
+                        [
+                            m.generate_p_value(
+                                true_null[v * len(self.metrics) + i],
+                                sample_size,
+                            )
+                            for i, m in enumerate(self.metrics)
+                        ]
+                    )
+
                 rejected = multipletests(p_values, alpha=self.alpha, method="fdr_bh")[0]
-                power.append(sum(rejected[~true_null]) / (m - m1))
+                power.append(sum(rejected[~true_null]) / (num_of_tests - m1))
 
         return float(np.mean(power))