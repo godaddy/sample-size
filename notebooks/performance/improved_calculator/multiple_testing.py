0ad28ae39aff1812447f5088f2c772fd8a972933
Vaughn <vjohnson1@godaddy.com>
2022-08-01 18:26:26 -0700

    initial commit

---
+++ notebooks/performance/improved_calculator/multiple_testing.py
@@ -0,0 +1,99 @@
+# +
+from typing import List
+
+import numpy as np
+from statsmodels.stats.multitest import multipletests
+
+from sample_size.metrics import BaseMetric
+import warnings
+
+
+class MultipleTestingMixin:
+    """
+    This class calculates sample size required under the case of multiple testing
+
+    Attributes:
+    metrics: a list of BaseMetric registered by users
+    variants: number of variants, including control
+    alpha: statistical significance
+    power: average power, calculated as #correct rejections/#true alternative hypotheses
+
+    """
+
+    metrics: List[BaseMetric]
+    alpha: float
+    power: float
+    variants: int
+
+    def get_multiple_sample_size(
+        self, lower: float, upper: float, replication: int, epsilon: float, max_recursion_depth: int, depth: int = 0,
+    ) -> int:
+        """
+        This method finds minimum required sample size per cohort that generates average power higher than required
+
+        Attributes:
+        lower: lower bound of sample size search; maximum of each metric's individually calculated sample size
+        upper: upper bound of sample size search; maximum of each metric's individually calculated sample size,
+        with Bonferroni adjustment(alpha = alpha/number of tests)
+        depth: number of recursions
+
+        Returns minimum required sample size per cohort
+        """
+
+        if depth > max_recursion_depth:
+            raise RecursionError(f"Couldn't find a sample size that satisfies the power you requested: {self.power}")
+
+        candidate = int(np.sqrt(lower * upper))
+        expected_power = self._expected_average_power(candidate, replication)
+        # print(f"lower: {lower}, upper: {upper}, expected_power: {expected_power}, candidate: {candidate}")
+        if np.isclose(self.power, expected_power, atol=epsilon):
+            return candidate
+        elif lower == upper:
+            raise RecursionError(f"Couldn't find a sample size that satisfies the power you requested: {self.power}")
+
+        if expected_power > self.power:
+            return self.get_multiple_sample_size(lower, candidate, replication, epsilon, max_recursion_depth, depth + 1)
+        else:
+            return self.get_multiple_sample_size(candidate, upper, replication, epsilon, max_recursion_depth, depth + 1)
+
+    def _expected_average_power(self, sample_size: int, replication) -> float:
+        """
+        This method calculates expected average power of multiple testings. For each possible number of true null
+        hypothesis, we simulate each metric/treatment variant's test statistics and calculate their p-values,
+        then calculate expected average power = number of True rejection/ true alternative hypotheses
+
+        Attributes:
+        sample size: determines the variance/ degrees of freedom of the distribution we sample test statistics from
+        replication: number of times we repeat the simulation process
+
+        Returns value expected average power
+        """
+        rejected_count = 0
+        true_alt_count = 0
+
+        # a metric for each test we would conduct
+        metrics = self.metrics * (self.variants - 1)
+
+        def fdr_bh(a):
+            return multipletests(a, alpha=self.alpha, method="fdr_bh")[0]
+        
+        power = []
+        for num_true_alt in range(1, len(metrics) + 1):
+            true_alt_count += num_true_alt * replication
+
+            true_alt = np.array([np.random.permutation(len(metrics)) < num_true_alt for _ in range(replication)]).T
+
+            p_values = []
+            for i, m in enumerate(metrics):
+                p_values.append(m.generate_p_values(true_alt[i], sample_size))
+
+            rejected = np.apply_along_axis(fdr_bh, 0, np.array(p_values))
+            
+            true_discoveries = rejected & true_alt
+
+            power.append(true_discoveries.sum() / true_alt.sum())
+        
+        return np.array(power).mean()
+# -
+
+
---
+++ notebooks/performance/legacy_calculator/calculator.py
@@ -0,0 +1,70 @@
+# +
+import json
+from pathlib import Path
+from typing import Any
+from typing import Dict
+from typing import List
+
+import numpy as np
+from jsonschema import validate
+
+from .metrics import BaseMetric
+from .metrics import BooleanMetric
+from .metrics import NumericMetric
+from .metrics import RatioMetric
+from .multiple_testing import MultipleTestingMixin
+
+DEFAULT_ALPHA = 0.05
+DEFAULT_POWER = 0.8
+DEFAULT_VARIANTS = 2
+
+
+class SampleSizeCalculator(MultipleTestingMixin):
+    """
+    This class is to calculate sample size based on metric type
+
+    Attributes:
+    alpha: statistical significance
+    power: statistical power
+
+    """
+
+    def __init__(self, alpha: float = DEFAULT_ALPHA, variants: int = DEFAULT_VARIANTS, power: float = DEFAULT_POWER):
+        self.alpha = alpha
+        self.power = power
+        self.metrics: List[BaseMetric] = []
+        self.variants: int = variants
+
+    def _get_single_sample_size(self, metric: BaseMetric, alpha: float) -> float:
+        effect_size = metric.mde / float(np.sqrt(metric.variance))
+        power_analysis = metric.power_analysis_instance
+        sample_size = int(
+            power_analysis.solve_power(
+                effect_size=effect_size, alpha=alpha, power=self.power, ratio=1, alternative="two-sided",
+            )
+        )
+        return sample_size
+
+    def get_sample_size(self, replication: int, epsilon: float, max_recursion_depth: int) -> float:
+        if len(self.metrics) * (self.variants - 1) < 2:
+            return self._get_single_sample_size(self.metrics[0], self.alpha)
+        lower = min([self._get_single_sample_size(metric, self.alpha) for metric in self.metrics])
+        upper = max(
+            [
+                self._get_single_sample_size(metric, self.alpha / (len(self.metrics) * (self.variants - 1)))
+                for metric in self.metrics
+            ]
+        )
+        return self.get_multiple_sample_size(lower, upper, replication, epsilon, max_recursion_depth)
+
+    def register_metrics(self, metrics: List[Dict[str, Any]]) -> None:
+        METRIC_REGISTER_MAP = {
+            "boolean": BooleanMetric,
+            "numeric": NumericMetric,
+            "ratio": RatioMetric,
+        }
+
+        for metric in metrics:
+            metric_class = METRIC_REGISTER_MAP[metric["metric_type"]]
+            registered_metric = metric_class(**metric["metric_metadata"])
+            self.metrics.append(registered_metric)
